17e9b5340d1dd6b4decee9bf12ffee6f66827730
Deeptanshu Sanghani <79523151+DeeptanshuSanghani1@users.noreply.github.com>
2024-02-09 10:22:16 -0500

    feat: Login, Register, Forgotpassword page templates (#9)
    layout: AuthLayout, ProtectedLayout, RootLayout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
+import RootLayout from './layouts/RootLayout.tsx'
+import AuthLayout from './layouts/AuthLayout.tsx'
+import ProtectedLayout from './layouts/ProtectedLayout.tsx'
+import Register from './pages/Authentication/Register.tsx'
+import Home from './pages/Home.tsx'
+import Login from './pages/Authentication/Login.tsx'
+import ForgotPassword from './pages/Authentication/ForgotPassword.tsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    element:<RootLayout/>,
+    children:[
+      {
+        element: <AuthLayout/>,
+        children:[
+        {
+          path:'/login',
+          element:<Login/>
+        },
+        {
+          path:'/register',
+          element:<Register/>
+        },
+        {
+          path:'/forgot-password',
+          element:<ForgotPassword/>
+        }
+      ],
+      },
+      {
+        element: <ProtectedLayout/>,
+        children:
+        [
+          {
+            path:"/",
+            element:<Home />,
+          },
+
+        ],
+      }
+    ],
+  },
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/Authentication/Login.tsx
@@ -0,0 +1,17 @@
+
+
+const Login = () => {
+    return (
+        <>
+        <div>
+            <h1>Login</h1>
+        </div>
+        <div>
+            <button>Register</button>
+            <button>Login</button>
+        </div>
+        </>
+    );
+};
+
+export default Login;
