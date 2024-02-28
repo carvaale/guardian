9f6d414595c17626a2e43e472b994aba5b2534e9
Imran Yafith <imranyafith@gmail.com>
2024-02-28 18:39:54 -0500

    Update authentication provider and dependencies

--- backend/providers/auth.py
+++ backend/providers/auth.py
@@ -7,10 +7,14 @@ from models import UserModels as Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from jwt import jwt, JWTError
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
-SECRET_KEY = dotenv_values(".env")['SECRET_KEY']
-ALGORITHM = dotenv_values(".env")['ALGORITHM']
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
@@ -24,31 +28,45 @@ def authenticate_user(email: str, password: str, db: Session):
         return False
     return user
 
+
 def create_access_token(user_id: int, email: str, expires_delta: timedelta):
     to_encode = {"sub": user_id, "email": email}
     expire = datetime.utcnow() + expires_delta
     encode = to_encode.update({"exp": expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
-def verify_token(token: Annotated[str,Depends(oauth2_bearer)]):
+
+def verify_token(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("email")
         user_id: int = payload.get("sub")
         if email is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
-        return {'user_id': user_id, 'email': email}
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+            )
+        return {"user_id": user_id, "email": email}
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
 
-async def get_current_user(token: Annotated[str,Depends(oauth2_bearer)]):
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     user_info = verify_token(token)
     if user_info is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
     return user_info
 
-def hash_password(password:str):
+
+def hash_password(password: str):
     return bcrypt_context.hash(password)
 
-def verify_password(plain_password:str, hashed_password:str):
+
+def verify_password(plain_password: str, hashed_password: str):
     return bcrypt_context.verify(plain_password, hashed_password)
