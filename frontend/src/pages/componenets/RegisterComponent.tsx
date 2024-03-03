7e97c281aecde55ab77ff0cee3cc51eb552ce55a
DeeptanshuSanghani1 <deeptanshusanghani@gmail.com>
2024-03-03 16:30:16 -0500

    changed text color for login/register/forgot-password forms

--- frontend/src/pages/componenets/RegisterComponent.tsx
+++ frontend/src/pages/componenets/RegisterComponent.tsx
@@ -49,14 +49,14 @@ export default function RegisterComponent() {
             <div className="rounded-[calc(1.5rem-1px)] px-10 p-12 bg-neutral-800">
               <div className="flex flex-col items-center">
                 <GuardianLogo />
-                <h1 className="text-xl text-center font-semibold text-gray-800 dark:text-white">Register your account</h1>
-                <p className="text-sm text-center tracking-wide text-gray-600 dark:text-gray-300">Already have an account? <a href="" onClick={()=> navigate("/login")} className="text-blue-600 transition duration-200 hover:underline dark:text-blue-400">Login</a></p>
+                <h1 className="text-xl text-center font-semibold text-gray-300 dark:text-white">Register your account</h1>
+                <p className="text-sm text-center tracking-wide text-gray-400 dark:text-gray-300">Already have an account? <a href="" onClick={()=> navigate("/login")} className="text-blue-600 transition duration-200 hover:underline dark:text-blue-400">Login</a></p>
               </div>
 
               <div className="mt-8 space-y-8">
                 <div className="space-y-6">
-                  <input className="w-full bg-transparent text-gray-600 dark:text-white dark:border-gray-700 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300" required placeholder="email" type="email" name="email"/>
-                  <input className="w-full bg-transparent text-gray-600 dark:text-white dark:border-gray-700 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300" required placeholder="Password" type="password" name="password"/>
+                  <input className="w-full bg-transparent text-gray-300 dark:text-white dark:border-gray-700 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300" required placeholder="email" type="email" name="email"/>
+                  <input className="w-full bg-transparent text-gray-300 dark:text-white dark:border-gray-700 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300" required placeholder="Password" type="password" name="password"/>
                 </div>
 
                 <button className="h-9 px-3 w-full bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:bg-blue-700 transition duration-500 rounded-md text-white" type="submit">
