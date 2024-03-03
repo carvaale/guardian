26c954be01872625e11b43b4b9ed0cb04d0504e4
DeeptanshuSanghani1 <deeptanshusanghani@gmail.com>
2024-03-03 16:40:56 -0500

    updated text/placeholder color

--- frontend/src/pages/PiiIdentifier.tsx
+++ frontend/src/pages/PiiIdentifier.tsx
@@ -21,7 +21,7 @@ const PiiIdentifier = () => {
                         <span className="text-white text-sm">I work at <span className="bg-orange-300">[REDACTED-PII]</span> and I'm building a <span className="bg-teal-300">[REDACTED-PII]</span> API that uses keys <span className="bg-purple-300">[REDACTED-PII]</span></span>
                     </div>
                     <div className="w-1/2 flex flex-col items-center gap-y-3">
-                        <textarea className="h-full w-full bg-transparent text-gray-600 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300" placeholder="Enter PII keywords here" name="pii" id="pii" />
+                        <textarea className="h-full w-full bg-transparent text-gray-400 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-400 invalid:border-red-500 dark:placeholder-gray-300" placeholder="Enter PII keywords here" name="pii" id="pii" />
                         <button className="h-9 px-3 w-1/2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:bg-blue-700 transition duration-500 rounded-md text-white">
                             Save PII Keys
                         </button>
