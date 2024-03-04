a6924e26512db1e4d350854d1f6e2f564baa4498
Imran Yafith <imranyafith@gmail.com>
2024-03-04 17:11:37 -0500

    refactor: use react-hook-form and zod resolver for login component

--- frontend/src/pages/components/LoginComponent.tsx
+++ frontend/src/pages/components/LoginComponent.tsx
@@ -1,111 +1,102 @@
-import { useRef, useState } from "react";
+import { z } from "zod";
+import GuardianLogo from "./Logo";
 import { useNavigate } from "react-router-dom";
-import { GuardianLogo } from "./Logo";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useAuth } from "../../hooks/useAuth";
 
-export default function LoginComponent() {
-  type LoginFormElements = {
-    email: HTMLInputElement;
-    password: HTMLInputElement;
-  };
+const LoginSchema = z.object({
+  email: z.string().min(1, "Email is required").email(),
+  password: z.string().min(1, "Password is required"),
+});
 
-  const [error, setError] = useState({ value: "" });
-  const { login } = useAuth();
+type LoginSchemaType = z.infer<typeof LoginSchema>;
+
+const LoginComponent = () => {
   const navigate = useNavigate();
-  const loginForm = useRef<HTMLFormElement & LoginFormElements>(null);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (loginForm.current) {
-      const email = loginForm.current.email.value;
-      const password = loginForm.current.password.value;
-      const loginInfo = { email, password };
+  const { login } = useAuth();
 
-      if (loginInfo.email === "" || loginInfo.password === "") {
-        setError({ value: "Please fill in all fields" });
-        return;
-      }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchemaType>({ resolver: zodResolver(LoginSchema) });
 
-      login(loginInfo)
-        .then((response) => {
-          console.log(response);
-          navigate("/");
-        })
-        .catch((err) => {
-          if (err.status === 401) {
-            setError({ value: "Invalid username or password" });
-          } else {
-            setError({ value: "An error occurred. Please try again later" });
-          }
-        });
-    } else {
-      console.error("Form not found ");
-    }
+  const onSubmit: SubmitHandler<LoginSchemaType> = (data: LoginSchemaType) => {
+    login(data)
+      .then((response) => {
+        console.log(response);
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.status === 401) {
+          console.log("Invalid username or password");
+        } else {
+          console.log("An error occurred. Please try again later");
+        }
+      });
   };
 
   return (
-    <>
-      <div
-        id="topo"
-        className="flex flex-row justify-evenly items-center w-full h-screen"
+    <div
+      id="topo"
+      className="flex flex-row justify-evenly items-center w-full h-screen"
+    >
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-[60%] md:w-[45%] lg:w-[30%]"
       >
-        <form action="" onSubmit={handleSubmit} ref={loginForm}>
-          <div className="rounded-[calc(1.5rem-1px)] px-10 p-12 bg-neutral-800">
-            <div className="flex flex-col items-center">
-              <GuardianLogo />
-              <h1 className="text-xl text-center font-semibold text-gray-300 dark:text-white">
-                Signin to your account
-              </h1>
-              <p className="text-sm text-center tracking-wide text-gray-400 dark:text-gray-300">
-                Don't have an account?{" "}
-                <a
-                  href=""
-                  onClick={() => navigate("/register")}
-                  className="text-blue-600 transition duration-200 hover:underline dark:text-blue-400"
-                >
-                  Signup
-                </a>
-              </p>
-            </div>
-
-            <div className="mt-8 space-y-8">
-              <div className="space-y-6">
-                <input
-                  className="w-full bg-transparent text-gray-300 dark:text-white dark:border-gray-700 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300"
-                  required
-                  placeholder="email"
-                  type="email"
-                  name="email"
-                />
-                <input
-                  className="w-full bg-transparent text-gray-300 dark:text-white dark:border-gray-700 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500 dark:placeholder-gray-300"
-                  required
-                  placeholder="Password"
-                  type="password"
-                  name="password"
-                />
-              </div>
-
-              <button
-                className="h-9 px-3 w-full bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:bg-blue-700 transition duration-500 rounded-md text-white"
-                type="submit"
+        <div className="rounded-xl px-10 p-12 bg-neutral-800">
+          <div className="flex flex-col items-center">
+            <GuardianLogo />
+            <h1 className="text-xl text-center font-semibold text-gray-300 dark:text-white">
+              Sign in to your account
+            </h1>
+            <p className="text-sm text-center tracking-wide text-gray-400">
+              Don't have an account?{" "}
+              <a
+                href=""
+                onClick={() => navigate("/register")}
+                className="text-blue-600 transition duration-200 hover:underline dark:text-blue-400"
               >
-                Login
-              </button>
-              {error && <p>{error.value}</p>}
-              <p className="text-sm tracking-wide text-gray-600 dark:text-gray-300">
-                <a
-                  href=""
-                  onClick={() => navigate("/forgot-password")}
-                  className="flex items-center justify-center text-blue-600 transition duration-200 hover:underline dark:text-blue-400"
-                >
-                  Forgot your password?
-                </a>
-              </p>
-            </div>
+                Sign up
+              </a>
+            </p>
           </div>
-        </form>
-      </div>
-    </>
+          <div className="flex flex-col justify-center items-center gap-6">
+            <div className="flex flex-col justify-center gap-4 mt-8 w-full">
+              <input
+                className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm"
+                placeholder="Email"
+                type="email"
+                {...register("email")}
+              />
+              {errors.email && (
+                <span className="text-red-600">{errors.email.message}</span>
+              )}
+              <input
+                className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm"
+                placeholder="Password"
+                type="password"
+                {...register("password")}
+              />
+              {errors.password && (
+                <span className="text-red-600">{errors.password.message}</span>
+              )}
+            </div>
+
+            <button
+              className="h-9 px-3 w-full bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:bg-blue-700 transition ease-in-out duration-200 rounded-md text-white"
+              type="submit"
+            >
+              Log in
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
-}
+};
+
+export default LoginComponent;
